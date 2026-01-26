f5714fdfa7bf3c158ceb5c78baba221692411c8f
ReshetnikovAV89 <andrey.reshetnikov.89@mail.ru>
2026-01-26 11:42:46 +0800

    fix: add order state and ignore dump folder

--- src/services/slices/constructorSlice.ts
+++ src/services/slices/constructorSlice.ts
@@ -1,5 +1,11 @@
-import { createSlice, nanoid, PayloadAction } from '@reduxjs/toolkit';
-import { TIngredient } from '../../utils/types';
+import {
+  createAsyncThunk,
+  createSlice,
+  nanoid,
+  PayloadAction
+} from '@reduxjs/toolkit';
+import { orderBurgerApi } from '../../utils/burger-api';
+import { TIngredient, TOrder } from '../../utils/types';
 
 type ConstructorItem = {
   id: string;
@@ -9,12 +15,24 @@ type ConstructorItem = {
 type ConstructorState = {
   bun: TIngredient | null;
   items: ConstructorItem[];
+  orderRequest: boolean;
+  orderModalData: TOrder | null;
 };
 
 const initialState: ConstructorState = {
   bun: null,
-  items: []
+  items: [],
+  orderRequest: false,
+  orderModalData: null
 };
+
+export const createOrder = createAsyncThunk<TOrder, string[]>(
+  'constructor/createOrder',
+  async (ingredientIds) => {
+    const res = await orderBurgerApi(ingredientIds);
+    return res.order;
+  }
+);
 
 const constructorSlice = createSlice({
   name: 'constructor',
@@ -37,11 +55,36 @@ const constructorSlice = createSlice({
     clearConstructor: (state) => {
       state.bun = null;
       state.items = [];
+      state.orderRequest = false;
+      state.orderModalData = null;
+    },
+    clearOrderModal: (state) => {
+      state.orderRequest = false;
+      state.orderModalData = null;
     }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createOrder.pending, (state) => {
+        state.orderRequest = true;
+        state.orderModalData = null;
+      })
+      .addCase(createOrder.fulfilled, (state, action) => {
+        state.orderRequest = false;
+        state.orderModalData = action.payload;
+      })
+      .addCase(createOrder.rejected, (state) => {
+        state.orderRequest = false;
+        state.orderModalData = null;
+      });
   }
 });
 
-export const { addIngredient, removeIngredient, clearConstructor } =
-  constructorSlice.actions;
+export const {
+  addIngredient,
+  removeIngredient,
+  clearConstructor,
+  clearOrderModal
+} = constructorSlice.actions;
 
 export const constructorReducer = constructorSlice.reducer;
